5f83bc908d61c2f0690f5f8ecabe9458437303ab
SASIDU <sasidunm@gmail.com>
2018-05-26 23:27:21 +0530

    movie description half done

--- app/src/main/java/com/example/sasidu/movie/data/GenreImages.java
+++ app/src/main/java/com/example/sasidu/movie/data/GenreImages.java
@@ -8,24 +8,24 @@ public class GenreImages {
     private static String[] ImageArray = {
             "http://www.filmsite.org/images/action-genre.jpg",
             "http://www.filmsite.org/images/adventure-genre.jpg",
+            "http://www.filmsite.org/images/animated-genre.jpg",
             "http://www.filmsite.org/images/comedy-genre.jpg",
             "http://www.filmsite.org/images/crime-genre.jpg",
-            "http://www.filmsite.org/images/action-genre.jpg",
-            "http://www.filmsite.org/images/adventure-genre.jpg",
+            "http://www.filmsite.org/images/documentary-genre.jpg",
+            "http://www.filmsite.org/images/drama-genre.jpg",
+            "http://www.filmsite.org/images/childrens-family-genre.jpg",
+            "http://www.filmsite.org/images/fantasy-genre.jpg",
+            "http://www.filmsite.org/images/epics-genre.jpg",
+            "http://www.filmsite.org/images/horror-genre.jpg",
+            "http://www.filmsite.org/images/musicals-genre.jpg",
+            "http://www.filmsite.org/images/detective-genre.jpg",
+            "http://www.filmsite.org/images/romance-genre.jpg",
+            "http://www.filmsite.org/images/scifi-genre.jpg",
             "http://www.filmsite.org/images/comedy-genre.jpg",
-            "http://www.filmsite.org/images/crime-genre.jpg",
-            "http://www.filmsite.org/images/action-genre.jpg",
-            "http://www.filmsite.org/images/adventure-genre.jpg",
-            "http://www.filmsite.org/images/comedy-genre.jpg",
-            "http://www.filmsite.org/images/crime-genre.jpg",
-            "http://www.filmsite.org/images/action-genre.jpg",
-            "http://www.filmsite.org/images/adventure-genre.jpg",
-            "http://www.filmsite.org/images/comedy-genre.jpg",
-            "http://www.filmsite.org/images/crime-genre.jpg",
-            "http://www.filmsite.org/images/action-genre.jpg",
-            "http://www.filmsite.org/images/adventure-genre.jpg",
-            "http://www.filmsite.org/images/comedy-genre.jpg",
-            "http://www.filmsite.org/images/crime-genre.jpg"
+            "http://www.filmsite.org/images/thriller-genre.jpg",
+            "http://www.filmsite.org/images/war-genre.jpg",
+            "http://www.filmsite.org/images/westerns-genre.jpg"
+
     };
 
     public static String getGenreImage(int imageNumber){
